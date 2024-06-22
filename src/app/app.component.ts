7830a379f0eb0df01633e082917f8fb9c17afb6c
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-22 12:48:25 -0500

    Se añade la propiedad city a la clase AppComponent y  se añade un conjuto {{}} a @Component .En src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   //templateUrl: './app.component.html',
-  template: ' <h1>Hello Universe</h1>',
+  template: 'Hello {{ city }}',
   //styleUrl: './app.component.css'
   styles: ':host {color: #a144eb;}'
 })
 export class AppComponent {
   title = 'EjercicioTutorial';
+  city = 'San Francisco';
 }
 

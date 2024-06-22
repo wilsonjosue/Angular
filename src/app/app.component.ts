abd10a9bf2cc5a82bc16d78ad4b2f6daf4735291
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-22 15:33:44 -0500

    Se realiza una prueba para verificar la integracion de dos componentes el principal y uno externo ubicado en src/app/user.En src/app/

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-user',
-  template: `
-    Username: {{ username }}
-  `,
-  standalone: true,
-})
-
-export class UserComponent {
-  username = 'youngTech';
-}
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [UserComponent],
+  imports: [RouterOutlet,UserComponent],
   //templateUrl: './app.component.html',
   template: `
     <section>
-      <h1>Welcome to Angular App</h1>
+      <h1>Welcome to Angular App desde {{city}}</h1>
       <app-user></app-user>
     </section>`,
   //styleUrl: './app.component.css'
   styles: ':host {color: #a144eb;}'
 })
 
-export class AppComponent {}
+export class AppComponent {
+  city = 'Peru'
+}

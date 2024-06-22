135143a242270effce1a8167fb205aec50d57b09
wilsonjosue <wturpoh@unsa.edu.pe>
2024-06-22 16:05:51 -0500

    Se crea un nuevo componente llamado games poara ptobar flujo de control for . En src/app/user/games.component.ts

---
+++ src/app/games/games.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games',
+  standalone: true,
+  imports: [],
+  template:`
+    <p>Welcome game works </p>
+  `,
+  styleUrl: './games.component.css'
+})
+export class GamesComponent {
+
+}
